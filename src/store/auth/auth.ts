604af79378df9f406dd72781d1b6e78602bbe05b
Alisher R. Salikhov <SalikhovA@HUMO.local>
2024-05-29 13:05:25 +0300

    one more bag fixed

--- src/store/auth/auth.ts
+++ src/store/auth/auth.ts
@@ -5,64 +5,72 @@ import { create } from "zustand";
 export interface IUserItem {
     userName: string,
     userEmail: string,
-    userPassword:string,
+    userPassword: string,
     userToken: string,
 }
 
-export interface IAuth{
-    isAuth:boolean,
-    currentUser:IUserItem[],
+export interface IAuth {
+    isAuth: boolean,
+    currentUser: IUserItem[],
 }
 
 type Actions = {
-    setAuth: (payload: boolean)=> void,
-    setCurrentUser: (payload: IUserItem[])=> void,
-    checkIsAuth: (payload: boolean)=> void,
-    logOut:()=> void
+    setAuth: (payload: boolean) => void,
+    setCurrentUser: (payload: IUserItem[]) => void,
+    checkIsAuth: (payload: boolean) => void,
+    logOut: () => void
 
 
 }
 
 
-const useAuth = create<IAuth & Actions >((set)=>({
-    isAuth:false,
-    currentUser:[],
-    setAuth:(payload)=> set(()=> ({isAuth: payload})),
-    setCurrentUser: (payload)=> set(()=> ({currentUser: payload})),
-    logOut: ()=> set(()=> ({isAuth: false,currentUser: []})),
-    checkIsAuth: (payload)=> set(()=> ({isAuth: payload}))
+const useAuth = create<IAuth & Actions>((set) => ({
+    isAuth: false,
+    currentUser: [],
+    setAuth: (payload) => set(() => ({ isAuth: payload })),
+    setCurrentUser: (payload) => set(() => ({ currentUser: payload })),
+    logOut: () => set(() => ({ isAuth: false, currentUser: [] })),
+    checkIsAuth: (payload) => set(() => ({ isAuth: payload }))
 
 }))
 
 export default useAuth
 
-export const handleLogin = (email:string, password:string, 
-    navigate:any, users:IUserItem[], setAuth:Function, setCurrentUser:Function) => { 
-        console.log(users);
-        
-        const loggedInUser = users === undefined ? [] : users.filter((user:IUserItem)=>{
-            return user.userEmail === email && user.userPassword === password
-        }) 
-        
-        if (loggedInUser.length > 0) {
-            setAuth(true)
-            setCurrentUser(loggedInUser)
-            setToStorage('loggedInUser', loggedInUser)
-            navigate.push('/')
-            Swal.fire({
-                title: "Loged in successful",
-                text: 'welcome to Humo Academy',
-                icon: "success"
-                
-            })
-        } else {
-            Swal.fire({
-                title: 'Login error!',
-                text: "This account doesn't exist",
-                icon: 'error',
-                confirmButtonText: 'try again'  
-              })
-        }
+export const handleLogin = (email: string, password: string,
+    navigate: any, users: IUserItem[], setAuth: Function, setCurrentUser: Function) => {
+    console.log(users);
+
+    // const loggedInUser = users === undefined ? [] : users.filter((user:IUserItem)=>{
+    //     return user.userEmail === email && user.userPassword === password
+    // }) 
+    let loggedInUser: IUserItem[] = []
+    if (users) {
+        users.forEach((user) => {
+            if (user.userEmail === email && user.userPassword === password) {
+                loggedInUser.push(user)
+            }
+        })
+    }
+
+    if (loggedInUser.length > 0) {
+        setAuth(true)
+        setCurrentUser(loggedInUser)
+        setToStorage('loggedInUser', loggedInUser)
+        navigate.push('/')
+        Swal.fire({
+            title: "Loged in successful",
+            text: 'welcome to Humo Academy',
+            icon: "success"
+
+        })
+    } else {
+        Swal.fire({
+            title: 'Login error!',
+            text: "This account doesn't exist",
+            icon: 'error',
+            confirmButtonText: 'try again'
+        })
+    }
 };
 
 
@@ -104,10 +112,10 @@ export const handleSignup = (email: string, fullName: string, password: string,
         // })
         // }
     }
-    
+
 
 }
 
-    
+
 
 
